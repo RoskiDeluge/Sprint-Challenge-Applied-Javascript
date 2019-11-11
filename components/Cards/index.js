de9ec481ecafee078729aff7e4738e5465ef10f6
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-11-10 17:57:04 -1000

    Midway through Cards index.js

--- components/Cards/index.js
+++ components/Cards/index.js
@@ -17,3 +17,49 @@
 // </div>
 //
 // Create a card for each of the articles and add the card to the DOM.
+
+const newCard = (name, photo, headline) => {
+  const card = document.createElement('div');
+  const cardHeadline = document.createElement('div');
+  const cardAuthor = document.createElement('div');
+  const cardImg = document.createElement('img');
+  const cardAuthorName = document.createElement('span');
+
+  card.appendChild(cardHeadline);
+  card.appendChild(cardAuthor);
+  cardAuthor.appendChild(cardImg);
+  cardAuthor.appendChild(cardAuthorName);
+
+  card.classList.add('card');
+  cardHeadline.classList.add('headline');
+  cardAuthor.classList.add('author');
+  
+  cardHeadline.textContent = headline;
+  cardImg.src = photo;
+  cardAuthorName = name;
+  
+
+  return card
+
+}
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/articles")
+  .then(response => {
+    console.log(response);
+    // const newArticlesB = response.data.articles.bootstrap;
+    // newTopics.forEach(topic => {
+    //   let li = document.createElement('div');
+    //   li.textContent = topic;
+    //   parentElement.appendChild(li);
+    // })
+  });
+
+  // data.articles.bootstrap Array 3 objects authorName, authorPhoto, headline
+  // data.articles.javascript
+  // data.articles.jquery
+  // data.articles.node
+  // data.articles.technology
+
+ 
+
