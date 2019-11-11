84c8de9c3e60b6cd1ba247d10636c3aea1b135bc
Roberto Delgado <robertodelgado@Robertos-MacBook-Air.local>
2019-11-10 16:36:05 -1000

    Finished Header index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,27 @@
 //  </div >
 // And add it to the DOM in the .header-container component
 
-function Header() {}
+function Header() {
+  const header = document.createElement('div');
+  const headerDate = document.createElement('span');
+  const headerMast = document.createElement('h1');
+  const headerTemp = document.createElement('span');
+  
+  header.appendChild(headerDate);
+  header.appendChild(headerMast);
+  header.appendChild(headerTemp);
+
+  header.classList.add('header');
+  headerDate.classList.add('date');
+  headerTemp.classList.add('temp');
+
+  headerDate.textContent = "MARCH 28, 2019";
+  headerMast.textContent = "Lambda Times";
+  headerTemp.textContent = "98°";
+
+  return header
+}
+
+const parent = document.querySelector('.header-container');
+
+parent.appendChild(Header());
